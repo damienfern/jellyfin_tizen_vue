bbd218cb3305d900b1f210e9f7b1218ec8931226
Damien Fernandes <damien.fernandes24@gmail.com>
2021-02-01 23:49:03 +0100

    begin router

--- src/App.vue
+++ src/App.vue
@@ -9,21 +9,7 @@
 
     <!-- Page content -->
     <div class="main">
-      <h1>Testing spatial</h1>
-
-      <div v-focus-section style="display: flex; flex-wrap: wrap;">
-          <h2>First list</h2>
-          <div class="listing">
-            <div v-for="index in 50" :key="index" @keypress.enter="goToDetails(index)" v-focus class="div" :style="{ 'border-color' : randomColor() }">{{ index }}</div>
-          </div>
-      </div>
-
-      <div v-focus-section>
-          <h2>Second list</h2>
-          <div class="listing">
-            <div v-for="index in 50" :key="index" @keypress.enter="goToDetails(index)" v-focus class="div" :style="{ 'border-color' : randomColor() }">{{ index }}</div>
-          </div>
-      </div>
+      <router-view></router-view>
     </div>
   </div>
 </template>
@@ -33,14 +19,6 @@
 export default {
   name: 'App',
   components: {
-  },
-  methods: {
-    randomColor () {
-      return '#' + Math.floor(Math.random()*16777215).toString(16)
-    },
-    goToDetails (index) {
-      console.log('clicked on ' + index);
-    }
   }
 }
 </script>
@@ -92,20 +70,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-.listing {
-  display: flex; 
-  flex-wrap: wrap;
-}
-
-.div {
-  height: 300px;
-  width: 300px;
-  border: 1px solid; 
-}
-
-:focus {
-  background-color: red;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+      <div v-focus-section style="display: flex; flex-wrap: wrap;">
+          <h2>First list</h2>
+          <div class="listing">
+            <div v-for="index in 50" :key="index" @keypress.enter="goToDetails(index)" v-focus class="div" :style="{ 'border-color' : randomColor() }">{{ index }}</div>
+          </div>
+      </div>
+
+      <div v-focus-section>
+          <h2>Second list</h2>
+          <div class="listing">
+            <div v-for="index in 50" :key="index" @keypress.enter="goToDetails(index)" v-focus class="div" :style="{ 'border-color' : randomColor() }">{{ index }}</div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  methods: {
+    randomColor () {
+      return '#' + Math.floor(Math.random()*16777215).toString(16)
+    },
+    goToDetails (index) {
+      console.log('clicked on ' + index);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.listing {
+  display: flex; 
+  flex-wrap: wrap;
+}
+
+.div {
+  height: 300px;
+  width: 300px;
+  border: 1px solid; 
+}
+
+:focus {
+  background-color: red;
+}
+</style>
